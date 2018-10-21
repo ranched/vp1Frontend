5ddca0cd833c027a545c8be87ae88930fbbe171d
Nolan Corcoran <nolan@nolans-mbp.attlocal.net>
2018-10-20 20:03:25 -0500

    added grunt config

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function(grunt) {
+ 
+  require('load-grunt-tasks')(grunt);
+ 
+ 
+  grunt.initConfig({
+    compress: {
+      main: {
+        options: {
+          archive: 'build.zip',
+          pretty: true
+        },
+        expand: true,
+        cwd: './',
+        src: ['./build/**/*'],
+        dest: './dist'
+      }
+    }
+  });
+ 
+  grunt.registerTask('default', ['compress']);
+};

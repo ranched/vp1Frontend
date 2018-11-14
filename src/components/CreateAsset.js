a9c887cba02466e5deb591dd24f216e171397472
Jason McLaughlin <jasonstephenm@gmail.com>
2018-11-14 09:54:51 -0600

    Add post requests to create asset page

--- src/components/CreateAsset.js
+++ src/components/CreateAsset.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Grid, Form, Button } from 'semantic-ui-react';
+import { Grid, Form, Button, Input, Label, Dropdown } from 'semantic-ui-react';
+import dropOptions from '../sample/dropOptions';
+import * as api from '../services/digitalAssets';
 
 const styles = {
   mainAssets: {
@@ -13,13 +15,103 @@ const options = [
   { key: 'a', text: 'Automobile', value: 'auto' }
 ];
 
+const capitalize = word => {
+  if (word === 'and') return word;
+  return word.charAt(0).toUpperCase() + word.slice(1);
+};
+
+const format = value => {
+  var text;
+  var words = value.split('_');
+  if (words.length === 1) {
+    text = capitalize(words[0]);
+  } else {
+    words = words.map(word => capitalize(word));
+    text = words.join(' ');
+  }
+  return { value, text };
+};
+
 class CreateAssets extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      scrmId: '',
+      title: '',
+      repo: '',
+      useCase: '',
+      archDiagram: '',
+      otubeLink: '',
+      industries: '',
+      cloudServices: '',
+      pillars: '',
+      description: '',
+      userEmail: '',
+      submitted: false
+    };
   }
 
-  //componentDidMount = () => {}
+  //componentDidMount = () => {};
+
+  handleChange = (e, { name, value }) => this.setState({ [name]: value });
+
+  handleSubmit = () => {
+    let assetObj = this.state;
+
+    // set up asset post 
+    const assetPostObj = {
+      scrm_id: assetObj.scrmId,
+      repo_url: assetObj.repo,
+      use_case: assetObj.useCase,
+      otube_url: assetObj.otubeLink,
+      description: assetObj.description
+    };
+
+    // make asset post
+    const assetPost = api.postAsset(assetPostObj)
+
+    //set up an industry post per industry and make posts
+    const industriesPosts = assetObj.industries.map(industry => (
+      {
+        industries: industry || 'example industry',
+        scrm_id: assetObj.scrmId || '123456'
+      })
+    ).map(postObj => api.postIndustry(postObj));
+
+
+    //set up a cloud service post per service and make posts
+    const cloudServicesPosts = assetObj.cloudServices.map(service => (
+      {
+        cloud_services: service || 'example service',
+        scrm_id: assetObj.scrmId || '123456'
+      })
+    ).map(postObj => api.postCloudService(postObj));
+
+    //set up a pillar post per pillar and make posts
+    const pillarsPosts = assetObj.pillars.map(pillar => (
+      {
+        cloud_services: pillar || 'example pillar',
+        scrm_id: assetObj.scrmId || '123456'
+      })
+    ).map(postObj => api.postPillar(postObj));
+
+
+    // set up user post object
+    const userPostObj = {
+      hubster_email: assetObj.userEmail || 'test.hubster@oracle.com',
+      hubster_name: assetObj.userName || 'Test Hubster',
+      scrm_id: assetObj.scrmId || '123456'
+    };
+
+    // make user post
+    const hubsterPost = api.postHubster(assetPostObj)
+
+
+    Promise.all([assetPost, industriesPosts, cloudServicesPosts, pillarsPosts, hubsterPost])
+      .catch(error => console.log(error));
+
+    //this.setState({ submitted: true });
+  };
 
   render() {
     return (
@@ -30,23 +122,123 @@ class CreateAssets extends Component {
           </Grid.Column>
           <Grid.Column width={10}>
             <Form>
-              <Form.Group widths="equal">
+              <Form.Field required>
+                <label>Select SCRM ID</label>
+                <Dropdown
+                  fluid
+                  selection
+                  label="SCRM ID"
+                  name="scrmId"
+                  value={this.state.scrmId}
+                  onChange={this.handleChange}
+                  options={dropOptions.scrmIds}
+                />
+              </Form.Field>
+              <Form.Field>
+                <label>Title</label>
+                <Input
+                  name="title"
+                  onChange={this.handleChange}
+                  value={this.state.title}
+                />
+              </Form.Field>
+              <Form.Field>
+                <label>Code Repository</label>
+                <Input
+                  name="repo"
+                  placeholder="Repository URL"
+                  onChange={this.handleChange}
+                  value={this.state.repo}
+                />
+              </Form.Field>
+              <Form.Field>
+                <label>Architecture Diagram</label>
+                <Input
+                  name="archDiagram"
+                  placeholder="Image URL"
+                  onChange={this.handleChange}
+                  value={this.state.archDiagram}
+                />
+              </Form.Field>
+              <Form.Field>
+                <label>OTube Link</label>
+                <Input
+                  name="otubeLink"
+                  placeholder="OTube URL"
+                  onChange={this.handleChange}
+                  value={this.state.otubeLink}
+                />
+              </Form.Field>
+              <Form.Field>
+                <label>Description</label>
+                <Input
+                  name="description"
+                  placeholder="Please enter a brief description"
+                  onChange={this.handleChange}
+                  value={this.state.description}
+                />
+              </Form.Field>
+              <Form.Field>
+                <label>Industries</label>
+                <Dropdown
+                  fluid
+                  scrolling
+                  multiple
+                  name="industries"
+                  value={this.state.industries}
+                  onChange={this.handleChange}
+                  options={dropOptions.industries.map(industry =>
+                    format(industry)
+                  )}
+                />
+              </Form.Field>
+              <Form.Field>
+                <label>Cloud Services</label>
+                <Dropdown
+                  fluid
+                  scrolling
+                  multiple
+                  name="cloudServices"
+                  value={this.state.cloudServices}
+                  onChange={this.handleChange}
+                  options={dropOptions.cloudServices.map(service =>
+                    format(service)
+                  )}
+                />
+              </Form.Field>
+              <Form.Field>
+                <label>Pillars</label>
+                <Dropdown
+                  fluid
+                  scrolling
+                  multiple
+                  name="pillars"
+                  value={this.state.pillars}
+                  onChange={this.handleChange}
+                  options={dropOptions.pillars.map(pillar => format(pillar))}
+                />
+              </Form.Field>
+              {/* <Form.Group widths="equal">
                 <Form.Input fluid label="SCRM ID" placeholder="Enter SCRM ID" />
               </Form.Group>
+
               <Form.Group widths="equal" className="update-form-field">
                 <Form.Input fluid label="ALM" placeholder="ALM" />
                 <Form.Button>Update</Form.Button>
               </Form.Group>
+
               <Form.Group widths="equal">
                 <Form.Input fluid label="USECASE" placeholder="Usecase" />
               </Form.Group>
+
               <Form.Group widths="equal">
                 <Form.Input fluid label="PICTURE" placeholder="Picture" />
-                {/* <Form.Input type="file" /> */}
               </Form.Group>
+
               <Form.Group widths="equal">
                 <Form.Input fluid label="OTUBE" placeholder="Otube" />
               </Form.Group>
+
               <Form.Group widths="equal">
                 <Form.Select
                   fluid
@@ -55,12 +247,11 @@ class CreateAssets extends Component {
                   placeholder="Industry"
                 />
               </Form.Group>
+
               <Form.TextArea
                 label="DESCRIPTION"
                 placeholder="Enter the description..."
-              />
-              <Form.TextArea label="README" placeholder="Add README" />
-              {/* <Form.Checkbox label='I agree to the Terms and Conditions' /> */}
+              /> */}
               <Form.Group>
                 <Form.Button primary>SUBMIT</Form.Button>
                 <Button as={Link} to="/">
--- src/services/digitalAssets.js
+++ src/services/digitalAssets.js
@@ -19,7 +19,7 @@ const format = value => {
   var text;
   var words = value.split('_');
   if (words.length === 1) { text = capitalize(words[0]); }
-  else { 
+  else {
     words = words.map(word => capitalize(word));
     text = words.join(' ');
   }
@@ -43,17 +43,17 @@ const getAssetDetails = (asset) => {
       return getPillars(scrm_id)
         .then(pillars => { asset['pillars'] = pillars; return asset; })
     })
-    .catch(error => { console.log(error); throw Error(error); } );
+    .catch(error => { console.log(error); throw Error(error); });
 }
 
 export const getAllAssets = () => {
   var reqUrl = apiUrl + 'assets';
-  return axios.get(reqUrl, {headers, auth})
+  return axios.get(reqUrl, { headers, auth })
     .then(result => result.data.items)
     .then(assets => {
       return assets.map(asset => {
         reqUrl = storageUrl + asset.arch_diagram_id;
-        return axios.get(reqUrl, {headers, auth, responseType: 'arraybuffer'})
+        return axios.get(reqUrl, { headers, auth, responseType: 'arraybuffer' })
           .then(result => Buffer.from(result.data, 'binary').toString('base64'))
           .then(result => 'data:image/png;base64,' + result)
           .then(arch_diagram => {
@@ -61,7 +61,7 @@ export const getAllAssets = () => {
             console.log(asset);
             return asset;
           })
-          .catch(error => { console.log(error); throw Error(error); } );
+          .catch(error => { console.log(error); throw Error(error); });
       })
     })
     .then(promises => Promise.all(promises))
@@ -71,50 +71,85 @@ export const getAllAssets = () => {
 }
 
 export const getAsset = (scrm_id) => {
-  var reqUrl = apiUrl + 'assets/'+scrm_id;
-  return axios.get(reqUrl, {headers, auth})
+  var reqUrl = apiUrl + 'assets/' + scrm_id;
+  return axios.get(reqUrl, { headers, auth })
     .then(result => result.data.items[0])
     .then(asset => {
       reqUrl = storageUrl + asset.arch_diagram_id;
-      return axios.get(reqUrl, {headers, auth, responseType: 'arraybuffer'})
+      return axios.get(reqUrl, { headers, auth, responseType: 'arraybuffer' })
         .then(result => Buffer.from(result.data, 'binary').toString('base64'))
         .then(result => 'data:image/png;base64,' + result)
         .then(arch_diagram => {
           asset['arch_diagram'] = arch_diagram;
           return asset;
         })
-        .catch(error => { console.log(error); throw Error(error); } );
+        .catch(error => { console.log(error); throw Error(error); });
     })
     .then(asset => { return getAssetDetails(asset); })
-    .catch(error => { console.log(error); throw Error(error); } );
+    .catch(error => { console.log(error); throw Error(error); });
 }
 
 export const getHubsters = (scrm_id) => {
-  var reqUrl = apiUrl + 'hubsters/'+scrm_id;
-  return axios.get(reqUrl, {headers, auth})
+  var reqUrl = apiUrl + 'hubsters/' + scrm_id;
+  return axios.get(reqUrl, { headers, auth })
     .then(result => result.data.items)
-    .catch(error => { console.log(error); throw Error(error); } );
+    .catch(error => { console.log(error); throw Error(error); });
 }
 
 export const getCloudServices = (scrm_id) => {
-  var reqUrl = apiUrl + 'cloud-services/'+scrm_id;
-  return axios.get(reqUrl, {headers, auth})
+  var reqUrl = apiUrl + 'cloud-services/' + scrm_id;
+  return axios.get(reqUrl, { headers, auth })
     .then(result => result.data.items.map(item => format(item.cloud_service)))
-    .catch(error => { console.log(error); throw Error(error); } );
+    .catch(error => { console.log(error); throw Error(error); });
 }
 
 export const getIndustries = (scrm_id) => {
-  var reqUrl = apiUrl + 'industries/'+scrm_id;
-  return axios.get(reqUrl, {headers, auth})
+  var reqUrl = apiUrl + 'industries/' + scrm_id;
+  return axios.get(reqUrl, { headers, auth })
     .then(result => result.data.items.map(item => format(item.industry)))
-    .catch(error => { console.log(error); throw Error(error); } );
+    .catch(error => { console.log(error); throw Error(error); });
 }
 
 export const getPillars = (scrm_id) => {
-  var reqUrl = apiUrl + 'pillars/'+scrm_id;
-  return axios.get(reqUrl, {headers, auth})
+  var reqUrl = apiUrl + 'pillars/' + scrm_id;
+  return axios.get(reqUrl, { headers, auth })
     .then(result => result.data.items.map(item => format(item.pillar)))
-    .catch(error => { console.log(error); throw Error(error); } );
+    .catch(error => { console.log(error); throw Error(error); });
+}
+
+export const postAsset = (assetObj) => {
+  var reqUrl = apiUrl + 'assets';
+  return axios.post(reqUrl, assetObj, { headers, auth })
+    .then(result => result.data)
+    .catch(error => { console.log(error); throw Error(error); });
+}
+
+export const postIndustry = (industryObj) => {
+  var reqUrl = apiUrl + 'industries';
+  return axios.post(reqUrl, industryObj, { headers, auth })
+    .then(result => result.data)
+    .catch(error => { console.log(error); throw Error(error); });
+}
+
+export const postCloudService = (cloudServiceObj) => {
+  var reqUrl = apiUrl + 'cloud-services';
+  return axios.post(reqUrl, cloudServiceObj, { headers, auth })
+    .then(result => result.data)
+    .catch(error => { console.log(error); throw Error(error); });
+}
+
+export const postPillar = (cloudServiceObj) => {
+  var reqUrl = apiUrl + 'pillars';
+  return axios.post(reqUrl, cloudServiceObj, { headers, auth })
+    .then(result => result.data)
+    .catch(error => { console.log(error); throw Error(error); });
+}
+
+export const postHubster = (userObj) => {
+  var reqUrl = apiUrl + 'hubsters';
+  return axios.post(reqUrl, userObj, { headers, auth })
+    .then(result => result.data)
+    .catch(error => { console.log(error); throw Error(error); });
 }
 
 /* .then(result => {return result.data.items},
